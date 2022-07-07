3e3a016d3c114656a99c1ae0de4b314968855770
Jacob McCurry <107898019+JacobMcCurry@users.noreply.github.com>
2022-07-06 20:45:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hi. Enter your name below:");
+var teacherName = Console.ReadLine();
+Console.WriteLine(value: $"Hello, {teacherName}! How are you today?");
+var teacherResponse = Console.ReadLine();
+if (teacherResponse != null) ;
